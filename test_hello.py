bfe726ef5549ccdbbb4794ad3d53505d9d2832a9
Noah Gift <noah.gift@gmail.com>
2020-08-17 15:27:41 +0000

    adding cli tests

--- test_hello.py
+++ test_hello.py
@@ -1,1 +1,10 @@
-"""This is a test"""
+from click.testing import CliRunner
+from hello import calculate
+
+
+    
+def test_calculate():
+  runner = CliRunner()
+  result = runner.invoke(calculate, ['--value1', '1', '--value2', '2'])
+  assert result.exit_code == 0
+  assert '2' in result.output
